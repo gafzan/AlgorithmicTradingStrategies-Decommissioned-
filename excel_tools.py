faf51faebd6eafee25e53d4f65b5f65a1d49a2ce
gafzan <gafzan@gmail.com>
2020-04-22 11:27:06 +0900

    able to save and load excel data using a full path

--- excel_tools.py
+++ excel_tools.py
@@ -32,11 +32,17 @@ __BORDER__ = Border(left=Side(border_style='thin', color='000000'), right=Side(b
                     top=Side(border_style='thin', color='000000'), bottom=Side(border_style='thin', color='000000'))
 
 
-def save_df(df_list: {list, pd.DataFrame}, workbook_name: str, folder_path: str, sheet_name_list: {str, list}=None):
+def save_df(df_list: {list, pd.DataFrame}, workbook_name: str = None, folder_path: str = None, full_path: str = None,
+            sheet_name_list: {str, list}=None):
     """Assumes df_list is a list of DataFrame, name is a string and sheet_name_list is a list of strings
     Saves all DataFrames in df_list to a sheet with the corresponding name in sheet_list"""
-    file_name = '\\' + workbook_name + '.xlsx'
-    writer = pd.ExcelWriter(folder_path + file_name, engine='xlsxwriter')
+
+    _check_save_load_input(workbook_name, folder_path, full_path)  # check the inputs
+
+    if full_path is None:
+        file_name = '\\' + workbook_name + '.xlsx'
+        full_path = folder_path + file_name
+    writer = pd.ExcelWriter(full_path, engine='xlsxwriter')
 
     if sheet_name_list is None:
         if type(df_list) == list:
@@ -65,43 +71,72 @@ def save_df(df_list: {list, pd.DataFrame}, workbook_name: str, folder_path: str,
     writer.save()  # close the Pandas Excel writer and output the excel file
 
 
-def load_df(workbook_name: str, folder_path: str, sheet_name: str = 'Sheet1', first_column_index=True):
+def load_df(workbook_name: str = None, folder_path: str = None, full_path: str = None, sheet_name: str = 'Sheet1',
+            first_column_index=True, sheet_name_error_handling: bool = True):
     """Assumes path_name and file_name are strings
     Returns a DataFrame"""
-    base_path = Path(folder_path)
-    excel_file_name = workbook_name + '.xlsx'
-    xl_file_fullname = base_path / excel_file_name
+
+    _check_save_load_input(workbook_name, folder_path, full_path)  # check the inputs
+
+    if full_path is None:
+        base_path = Path(folder_path)
+        excel_file_name = workbook_name + '.xlsx'
+        xl_file_fullname = base_path / excel_file_name
+    else:
+        xl_file_fullname = Path(full_path)
+
     if not xl_file_fullname.exists():
         print('No such file or directory!')
         sys.exit(-1)
-    try:
-        data = pd.read_excel(xl_file_fullname, sheet_name=sheet_name)
-    except xlrd.biffh.XLRDError:
-        # if the sheet name does not exist, ask the user to name one
-        workbook = load_workbook(xl_file_fullname)
-        if len(workbook.sheetnames) == 1:
-            logger.warning(
-                "'{}' does not exist. Loading '{}' sheet since it is the only sheet in the workbook.".format(
-                    sheet_name, workbook.sheetnames[0]))
-            data = pd.read_excel(xl_file_fullname, sheet_name=workbook.sheetnames[0])
-        else:
-            print(f"'{sheet_name}' does not exist. Chose one sheet from below:")
-            for sht_name in workbook.sheetnames:
-                print(f"{workbook.sheetnames.index(sht_name) + 1}: {sht_name}")
-            ask_user = True
-            while ask_user:
-                try:
-                    number = int(input(f"Input a number between 1 and {len(workbook.sheetnames)}: "))
-                    if number < 0:
-                        raise ValueError
-                    sheet_name = workbook.sheetnames[number - 1]
-                    ask_user = False
-                except (ValueError, IndexError):  # catch all errors
-                    pass
+
+    if sheet_name_error_handling:
+        try:
             data = pd.read_excel(xl_file_fullname, sheet_name=sheet_name)
+        except xlrd.biffh.XLRDError:
+            # if the sheet name does not exist, ask the user to name one
+            workbook = load_workbook(xl_file_fullname)
+            if len(workbook.sheetnames) == 1:
+                logger.warning(
+                    "'{}' does not exist. Loading '{}' sheet since it is the only sheet in the workbook.".format(
+                        sheet_name, workbook.sheetnames[0]))
+                data = pd.read_excel(xl_file_fullname, sheet_name=workbook.sheetnames[0])
+            else:
+                print(f"'{sheet_name}' does not exist. Chose one sheet from below:")
+                for sht_name in workbook.sheetnames:
+                    print(f"{workbook.sheetnames.index(sht_name) + 1}: {sht_name}")
+                ask_user = True
+                while ask_user:
+                    try:
+                        number = int(input(f"Input a number between 1 and {len(workbook.sheetnames)}: "))
+                        if number < 0:
+                            raise ValueError
+                        sheet_name = workbook.sheetnames[number - 1]
+                        ask_user = False
+                    except (ValueError, IndexError):  # catch all errors
+                        pass
+                data = pd.read_excel(xl_file_fullname, sheet_name=sheet_name)
+    else:
+        data = pd.read_excel(xl_file_fullname, sheet_name=sheet_name)
     if first_column_index:
         data.set_index(list(data)[0], inplace=True, drop=True)
     return data
+
+
+def _check_save_load_input(workbook_name: str = None, folder_path: str = None, full_path: str = None)-> None:
+    """
+    Checks so only full_path or both workbook_name and folder_path is provided.
+    :param workbook_name: string
+    :param folder_path: string
+    :param full_path: string with full path including the file type (e.g. xlsx)
+    :return:
+    """
+    if full_path is None and not (workbook_name is not None and folder_path is not None):
+        raise ValueError("Please provide both workbook_name and folder_path.")
+    elif full_path is not None and (workbook_name is not None or folder_path is not None):
+        raise ValueError("Please only provide full_path.")
+    elif full_path is None and workbook_name is None and folder_path is None:
+        raise ValueError("Please only provide full_path or both workbook_name and folder_path.")
+    return
 
 
 def excel_files_in_directory(directory: {Path, str}) -> list:
