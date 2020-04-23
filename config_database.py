b30ee570d3575e8475a84f74a435ede93719aa02
gafzan <gafzan@gmail.com>
2020-04-23 09:26:50 +0900

    added base_folder and changed some format

--- config_database.py
+++ config_database.py
@@ -13,15 +13,16 @@ def create_directory(directory: str)-> None:
 
 
 # folders for accessing and saving data
+base_folder = r'C:\Users\{}\PycharmProjects\AlgorithmicTradingStrategies'.format(user_name)
+create_directory(base_folder)
 # contains excel workbooks with tickers
-excel_ticker_folder = r'C:\Users\{}\PycharmProjects\AlgorithmicTradingStrategies\excel_data\tickers'.format(user_name)
+excel_ticker_folder = base_folder + r'\excel_data' + '\\tickers'
 create_directory(excel_ticker_folder)
 # contains excel workbooks with data requested from the database
-data_request_folder = r'C:\Users\{}\PycharmProjects\AlgorithmicTradingStrategies\excel_data\data_requests'.format(user_name)
+data_request_folder = base_folder + r'\excel_data' + '\\data_requests'
 create_directory(data_request_folder)
 # contains excel workbooks used to insert data into the database
-excel_files_to_feed_database_folder = r'C:\Users\{}\PycharmProjects\AlgorithmicTradingStrategies\excel_data\excel_based_feeder_workbooks'.format(user_name)
+excel_files_to_feed_database_folder = base_folder + r'\excel_data' + '\\excel_based_feeder_workbooks'
 create_directory(excel_files_to_feed_database_folder)
 
-my_database_name = r'sqlite:///C:\Users\{}\PycharmProjects\AlgorithmicTradingStrategies\financial_database_v1.db'.format(user_name)
-
+my_database_name = r'sqlite:///' + base_folder + '\\financial_database_v1.db'
