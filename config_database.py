0150784f3845490f2500dcf48bb52e596f54dd49
gafzan <gafzan@gmail.com>
2020-07-11 08:00:14 +0900

    changed location of database files

--- config_database.py
+++ config_database.py
@@ -27,5 +27,4 @@ create_directory(excel_files_to_feed_database_folder)
 # contains excel workbooks with back test data
 back_test_folder = base_folder + r'\excel_data' + '\\back_tests'
 create_directory(back_test_folder)
-
-my_database_name = r'sqlite:///' + base_folder + '\\financial_database_v1.db'
+my_database_name = r'sqlite:///' + base_folder + '\\database\database_files\\financial_database_v1.db'
