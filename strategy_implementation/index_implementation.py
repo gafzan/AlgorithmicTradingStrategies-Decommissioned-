337bd52bc6fad0303c8b864e1f93a028c8eb66f5
gafzan <gafzan@gmail.com>
2020-07-11 15:30:48 +0900

    initial commit

---
+++ strategy_implementation/index_implementation.py
@@ -0,0 +1,83 @@
+"""
+index_implementation.py
+"""
+from matplotlib import pyplot as plt
+
+# my modules
+from strategy_implementation.index import Index
+from strategy_implementation.strategy_weight import EqualWeight, VolatilityWeight
+from strategy_implementation.strategy_signal import VolatilityRankSignal
+from strategy_implementation.investment_universe import InvestmentUniverse
+
+from financial_analysis.finance_tools import return_and_risk_analysis
+from excel_tools import save_df, format_risk_return_analysis_workbook
+from database.config_database import back_test_folder, my_database_name
+from database.financial_database import FinancialDatabase
+from dataframe_tools import merge_two_dataframes_as_of
+
+
+def main():
+    # parameters
+    benchmark_ticker = None
+    print_result = True
+    plot_result = True
+    save_results = False
+
+    # define the investment universe
+    tickers = ['AZN.ST', 'SAND.ST', 'ABB.ST']
+    liquid_stock_universe = InvestmentUniverse(tickers=tickers, start='2018', end='2020', freq='3M')
+    liquid_stock_universe.apply_liquidity_filter(60, 300000)
+
+    # setup the index
+    low_vol_eqw_index = Index(investment_universe=liquid_stock_universe)
+    low_vol_eqw_index.weight = EqualWeight(gross_equal_weights=False)
+    index_reporting(low_vol_eqw_index, print_results=print_result, save_results=save_results, plot_results=plot_result,
+                    benchmark_ticker=benchmark_ticker)
+
+
+def index_reporting(index_: Index, index_name: str = 'TEST', benchmark_ticker: str = None, save_results: bool = True,
+                    print_results: bool = True, plot_results: bool = False):
+    """
+    Calculates the back test of an instance of Index object and prints, saves and plots results when applicable.
+    :param index_: Index
+    :param index_name: str
+    :param benchmark_ticker: str
+    :param save_results: bool
+    :param print_results: bool
+    :param plot_results: bool
+    :return: None
+    """
+    # perform back test and add nem as a column head
+    back_test_result = index_.get_back_test()
+    try:
+        index_back_test = back_test_result.loc[:, ['NET_INDEX']].dropna()
+    except KeyError:
+        index_back_test = back_test_result.loc[:, ['GROSS_INDEX']].dropna()
+    index_name = index_name.upper() + '_INDEX'
+    index_back_test.columns = [index_name]
+
+    if benchmark_ticker:
+        # download price for the benchmark
+        fin_db = FinancialDatabase(my_database_name)
+        benchmark_price = fin_db.get_close_price_df(benchmark_ticker)
+        index_back_test = merge_two_dataframes_as_of(index_back_test, benchmark_price)
+        # normalize the benchmark price to start at the same value as the index
+        index_back_test.iloc[:, -1] = index_back_test.iloc[0, -2] * index_back_test.iloc[:, -1] / index_back_test.iloc[0, -1]
+
+    # store each DataFrame with results as a dictionary
+    performance_data = {'Description': index_.get_index_desc_df()}
+    performance_data.update(return_and_risk_analysis(index_back_test, benchmark_ticker is not None, print_results))
+    performance_data.update({'Back test': index_back_test})
+    if save_results:
+        save_df(list(performance_data.values()), workbook_name=index_name, folder_path=back_test_folder,
+                sheet_name_list=list(performance_data.keys()))
+        format_risk_return_analysis_workbook(back_test_folder + '\\' + index_name + '.xlsx')
+    if plot_results:
+        index_back_test.plot()
+        plt.show()
+
+
+if __name__ == '__main__':
+    main()
+
+
