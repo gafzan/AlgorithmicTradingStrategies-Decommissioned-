17e0dc68794c2e05f6278206ebc7b4644a6fae27
gafzan <gafzan@gmail.com>
2020-10-25 16:52:40 +0900

    initial commit (newer version)

--- algorithmic_strategy/strategy.py
+++ algorithmic_strategy/strategy.py
@@ -11,8 +11,7 @@ from database.financial_database import FinancialDatabase
 from database.config_database import my_database_name
 from algorithmic_strategy.investment_universe import InvestmentUniverse
 from dataframe_tools import merge_two_dataframes_as_of
-from financial_analysis.finance_tools import index_daily_rebalanced
-from algorithmic_strategy import strategy_weight, strategy_signal
+from algorithmic_strategy import strategy_weight, strategy_signal, strategy_weight_adjustments
 
 # Logger
 logger = logging.getLogger(__name__)
@@ -60,8 +59,8 @@ class Basket:
             raise ValueError('dividend_tax needs to be greater or equal to zero.')
 
     def get_desc(self):
-        return "currency = {currency}, {total_return}{dividend_tax}"\
-            .format(currency=self.currency if self.currency else 'local',
+        return "{currency}, {total_return}{dividend_tax}"\
+            .format(currency=self.currency.upper() if self.currency else 'local currency',
                     total_return='total return' if self.total_return else 'price return',
                     dividend_tax=' with ' + str(round(self.dividend_tax*100, 2)) + '% dividend tax' if self.dividend_tax and self.total_return else '')
 
@@ -72,11 +71,10 @@ class Basket:
 class Index(Basket):
     """Class definition of Index"""
 
-    def __init__(self, investment_universe: InvestmentUniverse, signal=None, weight=None, index_fee: float = 0.0,
+    def __init__(self, investment_universe: InvestmentUniverse, signal=None, weight=None, weight_modifier=None, index_fee: float = 0.0,
                  transaction_cost: float = 0.0, currency: str = None, total_return: bool = False,
                  dividend_tax: float = 0.0, observation_calendar: pd.DatetimeIndex = None, initial_value: float = 100.0,
-                 daily_rebalancing: bool = True, weight_rebalance_lag: int = 1, weight_observation_lag: int = 2,
-                 volatility_target: float = None, volatility_observation_lag: {int, list}=None, risky_weight_cap: float = 1.0):
+                 strategy_weight_smoothing: int = 1, weight_observation_lag: int = 2):
         super().__init__(investment_universe=investment_universe, currency=currency, total_return=total_return,
                          dividend_tax=dividend_tax)
         self.signal = signal
@@ -85,52 +83,100 @@ class Index(Basket):
         self.transaction_cost = transaction_cost
         self.initial_value = initial_value
         self.observation_calendar = observation_calendar
-        self.daily_rebalancing = daily_rebalancing
         self.weight_observation_lag = weight_observation_lag
-        self.weight_rebalance_lag = weight_rebalance_lag
-        self.volatility_target = volatility_target
-        self.volatility_observation_lag = volatility_observation_lag
-        self.risky_weight_cap = risky_weight_cap
+        self.strategy_weight_smoothing = strategy_weight_smoothing
 
-    def get_back_test(self, end_date: datetime = None, only_index: bool = False):
+        self.weight_modifier = weight_modifier
+
+    def _apply_weight_modifier(self, daily_return_df: pd.DataFrame, daily_weight_df: pd.DataFrame):
+
+        if self.weight_modifier is None:
+            pass
+        elif isinstance(self.weight_modifier, list):
+            # apply each weight overlays
+            for weight_overlay in self.weight_modifier:
+                daily_return_df, daily_weight_df = weight_overlay.get_return_weight_tuple_after_scaling(
+                    multivariate_daily_return_df=daily_return_df,
+                    multivariate_daily_weight_df=daily_weight_df)
+        else:
+            daily_return_df, daily_weight_df = self.weight_modifier.get_return_weight_tuple_after_scaling(multivariate_daily_return_df=daily_return_df, multivariate_daily_weight_df=daily_weight_df)
+        return daily_return_df, daily_weight_df
+
+    def get_back_test(self, end_date: datetime = None, drop_nan: bool = True) -> pd.DataFrame:
+        """
+        Calculate the index using the signal, weight and the weight overlays
+        :param end_date: datetime
+        :param drop_nan: bool
+        :return: pd.DataFrame
+        """
         weight_df = self.get_weight()
         price_df = self.basket_prices(start_date=weight_df.index[0], end_date=end_date)
         daily_returns = price_df.pct_change()
-        index_result = index_daily_rebalanced(daily_returns, weight_df, self.transaction_cost, self.index_fee,
-                                              self.weight_rebalance_lag, self.weight_observation_lag,
-                                              self.initial_value,
-                                              self.volatility_target, self.volatility_observation_lag,
-                                              self.risky_weight_cap)
-        if only_index:
-            try:
-                return index_result.loc[:, ['NET_INDEX']].dropna()
-            except KeyError:
-                return index_result.loc[:, ['GROSS_INDEX']].dropna()
-        else:
-            return index_result
+
+        # calculate the daily weights and apply smoothing
+        daily_weight = merge_two_dataframes_as_of(pd.DataFrame(index=price_df.index), weight_df)
+        daily_weight = daily_weight.rolling(window=self.strategy_weight_smoothing, min_periods=1).mean()
+
+        # apply the weight adjustments (volatility target and Beta hedge etc.)
+        daily_returns, daily_weight = self._apply_weight_modifier(daily_return_df=daily_returns, daily_weight_df=daily_weight)
+
+        # TODO need to check if the weight obs lag is ok
+
+        # clean the daily returns
+        daily_returns.fillna(0, inplace=True)
+
+        # calculate the gross return of the index
+        col_name = 'gross_index'
+        gross_instrument_returns = daily_returns * daily_weight.shift(self.weight_observation_lag).values
+        index_return_df = pd.DataFrame({'gross_return': gross_instrument_returns.sum(axis=1, skipna=False).values},
+                                       index=gross_instrument_returns.index)
+
+        # if applicable, calculate the return net of transaction costs and fees
+        if self.transaction_cost > 0 or self.index_fee != 0:
+            # net of transaction costs
+            abs_weight_delta = daily_weight.diff().abs().sum(axis=1)
+            transaction_cost_df = pd.DataFrame({'TC': self.transaction_cost * abs_weight_delta.values},
+                                               index=abs_weight_delta.index)
+            index_return_df -= transaction_cost_df.values
+
+            # net of index fees
+            dt = [None] + [(index_return_df.index[n] - index_return_df.index[n - 1]).days / 365 for n in
+                           range(1, len(index_return_df.index))]
+            dt_s = pd.Series(data=dt, index=index_return_df.index)
+            index_fee_df = pd.DataFrame({'FEE': self.index_fee * dt_s.values}, index=dt_s.index)
+            index_return_df -= index_fee_df.values
+            index_return_df.columns = ['net_return']
+            col_name = 'net_index'
+
+        # calculate the index
+        index_result_df = index_return_df.copy()
+        start_of_index_i = index_return_df.index.get_loc(index_return_df.first_valid_index()) - 1
+        index_result_df = (1 + index_result_df).cumprod()  # (1 + r_1) * (1 + r_2) * ...
+        index_result_df.iloc[start_of_index_i, :] = 1.0  # start at 1.0
+        index_result_df *= self.initial_value  # scale the index by the initial value
+        index_result_df.columns = [col_name]
+        if drop_nan:
+            index_result_df.dropna(inplace=True)
+        return index_result_df
 
     def get_index_desc_df(self):
+        """
+        Creates a DataFrame where all rows represents descriptions of the index components
+        :return: pd.DataFrame
+        """
         index_desc_df = pd.DataFrame(columns=['Index description'])
         index_desc_df.loc['Index eligibility:'] = self.investment_universe.get_desc()
         index_desc_df.loc['Currency:'] = self.currency.upper() if self.currency else 'Local currency'
         index_desc_df.loc['Return type:'] = 'Total return' if self.total_return else 'Price return'
         if self.dividend_tax:
             index_desc_df.loc['Dividend tax:'] = str(round(100 * self.dividend_tax, 2)) + '%'
-        if issubclass(type(self.signal), strategy_signal.Signal):
-            index_desc_df.loc['Signal:'] = type(self.signal).__name__
-        index_desc_df.loc['Weight:'] = type(self.weight).__name__
+        if self.signal is not None:
+            index_desc_df.loc['Signal:'] = type(self.signal).__name__ + ' (' + self.signal.get_desc() + ')'
+        index_desc_df.loc['Weight:'] = type(self.weight).__name__ + ' (' + self.weight.get_weight_desc() + ')'
         index_desc_df.loc['Index fee p.a.:'] = str(round(100 * self.index_fee, 2)) + '%'
         index_desc_df.loc['Transaction costs:'] = str(round(100 * self.transaction_cost, 2)) + '%'
-        if self.volatility_target:
-            index_desc_df.loc['Volatility target:'] = str(round(100 * self.volatility_target, 2)) + '%'
-            if isinstance(self.volatility_observation_lag, list):
-                obs_lag_list = [str(obs_lag) for obs_lag in self.volatility_observation_lag]
-                obs_lag_desc = 'Max volatility observed over %s days' % ', '.join(obs_lag_list)
-            else:
-                obs_lag_desc = 'Observed over {} days'.format(self.volatility_observation_lag)
-            index_desc_df.loc['Volatility observation lag:'] = obs_lag_desc
-            index_desc_df.loc['Maximum risky weight:'] = str(round(100 * self.risky_weight_cap, 2)) + '%'
-        index_desc_df.loc['Weight smoothing (days):'] = str(self.weight_rebalance_lag)
+        if self.strategy_weight_smoothing > 1:
+            index_desc_df.loc['Weight smoothing (days):'] = str(self.strategy_weight_smoothing)
         index_desc_df.loc['Weight observation lag (days):'] = str(self.weight_observation_lag)
         return index_desc_df
 
@@ -153,7 +199,7 @@ class Index(Basket):
             return self.signal.get_signal()
 
     def get_desc(self):
-        return super().get_desc() + ', VT = {}%'.format(round(100 * self.volatility_target, 2)) if self.volatility_target else ''
+        return super().get_desc()
 
     # ------------------------------------------------------------------------------------------------------------------
     # getter and setter methods
@@ -182,6 +228,24 @@ class Index(Basket):
             self._weight = weight
         else:
             raise ValueError('Needs to be of type that is a subclass of Weight.')
+
+    @property
+    def weight_modifier(self):
+        return self._weight_modifier
+
+    @weight_modifier.setter
+    def weight_modifier(self, weight_modifier):
+        if weight_modifier is None:
+            self._weight_modifier = None
+        elif issubclass(type(weight_modifier), strategy_weight_adjustments._WeightAdjustment):
+            self._weight_modifier = weight_modifier
+        elif isinstance(weight_modifier, list):
+            if all(issubclass(type(w_mod), strategy_weight_adjustments._WeightAdjustment) for w_mod in weight_modifier):
+                self._weight_modifier = weight_modifier
+            else:
+                raise ValueError('All elements in the weight_modifier list is not a subclass of _WeightAdjustment')
+        else:
+            raise ValueError('weight_modifier needs to be an object or a list of objects that are a subclass of _WeightAdjustment.')
 
     @property
     def index_fee(self):
@@ -244,58 +308,40 @@ class Index(Basket):
         self._weight_observation_lag = weight_observation_lag
 
     @property
-    def weight_rebalance_lag(self):
-        return self._weight_rebalance_lag
+    def strategy_weight_smoothing(self):
+        return self._strategy_weight_smoothing
 
-    @weight_rebalance_lag.setter
-    def weight_rebalance_lag(self, weight_rebalance_lag: int):
-        if weight_rebalance_lag < 1:
-            raise ValueError('weight_rebalance_lag needs to be an int larger or equal to 1.')
-        self._weight_rebalance_lag = weight_rebalance_lag
-
-    @property
-    def volatility_target(self):
-        return self._volatility_target
-
-    @volatility_target.setter
-    def volatility_target(self, volatility_target: float):
-        if volatility_target is None:
-            self._volatility_target = None
-        elif volatility_target <= 0:
-            raise ValueError('volatility_target needs to be a float larger than 0.')
-        self._volatility_target = volatility_target
-
-    @property
-    def risky_weight_cap(self):
-        return self._risky_weight_cap
-
-    @risky_weight_cap.setter
-    def risky_weight_cap(self, risky_weight_cap: float):
-        if risky_weight_cap <= 0:
-            raise ValueError('risky_weight_cap needs to be a float larger than 0.')
-        self._risky_weight_cap = risky_weight_cap
+    @strategy_weight_smoothing.setter
+    def strategy_weight_smoothing(self, strategy_weight_smoothing: int):
+        if strategy_weight_smoothing < 1:
+            raise ValueError('strategy_weight_smoothing needs to be an int larger or equal to 1.')
+        self._strategy_weight_smoothing = strategy_weight_smoothing
 
 
 def main():
-    # investment universe
-    tickers = ["AGRO.ST", "AAK.ST", "ABB.ST"]
-    invest_uni = InvestmentUniverse(tickers, '2018', '2020', freq='M')
-    invest_uni.apply_liquidity_filter(60, 300000, 'sek')
 
-    index = Index(invest_uni, transaction_cost=0.001, index_fee=0.005, volatility_target=0.1,
-                  volatility_observation_lag=60)
-    index.weight = strategy_weight.EqualWeight()
-    eqw_index = index.get_back_test(only_index=True)
-    index.weight = strategy_weight.VolatilityWeight(60)
-    inv_vol_index = index.get_back_test(only_index=True)
-    combined = eqw_index.join(inv_vol_index, rsuffix='_inverse_volatility'.upper())
+    tickers = ['spy', 'bnd', 'govt', 'vwo']
+    from algorithmic_strategy.investment_universe import InvestmentUniverse
+    obs_cal = pd.date_range('2012', '2019', freq='3M')
 
+    inv_uni = InvestmentUniverse(tickers, observation_calendar=obs_cal)
+    inv_uni.apply_published_close_price_filter(max_number_days_since_publishing=5)
+    perf_signal = strategy_signal.PerformanceRankSignal(30, rank_number=3)
+    eqw = strategy_weight.EqualWeight()
 
-    from matplotlib import pyplot as plt
-    print(combined)
-    combined.plot()
-    plt.show()
+    vt_overlay = strategy_weight_adjustments.VolatilityControl(vol_control_level=0.05, vol_lag=20)
+    beta_overlay = strategy_weight_adjustments.BetaHedge('vt', beta_lag=50)
+
+    momentum_index = Index(investment_universe=inv_uni, signal=perf_signal, weight=eqw, weight_observation_lag=2,
+                           transaction_cost=0.0, index_fee=0.0, weight_modifier=[beta_overlay, vt_overlay])
+
+    result = momentum_index.get_back_test()
+    result.to_clipboard()
+    print(momentum_index)
 
 
 if __name__ == '__main__':
     main()
+
+
+
